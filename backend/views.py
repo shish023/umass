ed4778da27c7f29523f5bc64b4b716dc3036b0da
Shishir <shish.inferno@gmail.com>
2015-10-24 05:03:45 -0400

    requirements changed + send, receive

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def send(request):
+
+    return render(request, "send.html", {})
+
+
+def receive(request):
+
+    data = request.GET['text']
+
+    print data
+
+    content = {
+        "data": data,
+    }
+
+    return render(request, "receive.html", content)
--- umass/urls.py
+++ umass/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^send/', 'backend.views.send', name='send'),
+    url(r'^receive/', 'backend.views.receive', name='receive'),
 ]
